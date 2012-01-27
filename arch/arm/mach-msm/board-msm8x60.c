ebc07d6b0527492cba9d75d60685832f990212f5
James Sullins <jcsullins@gmail.com>
2012-01-27 03:06:40 -0600

    tenderloin: do not setup pm8058 keypad

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -10794,7 +10794,7 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 		pm8058_platform_data.keypad_pdata = &fluid_keypad_data;
 	else if (machine_is_msm8x60_dragon())
 		pm8058_platform_data.keypad_pdata = &dragon_keypad_data;
-	else
+	else if (!machine_is_tenderloin())
 		pm8058_platform_data.keypad_pdata = &ffa_keypad_data;
 #ifndef CONFIG_MSM_CAMERA_V4L2
 	/* Specify reset pin for OV9726 */
