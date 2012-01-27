a9347bf3d3af0aca446ece51682733517e6e2235
James Sullins <jcsullins@gmail.com>
2012-01-27 02:02:41 -0600

    add ifdefs: CONFIG_GPIO_SX150X, CONFIG_PMIC8058_OTHC

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -2938,6 +2938,8 @@ static struct platform_device android_pmem_smipool_device = {
 #endif
 
 #define GPIO_DONGLE_PWR_EN 258
+
+#if defined(CONFIG_GPIO_SX150X) || defined(CONFIG_GPIO_SX150X_MODULE)
 static void setup_display_power(void);
 static int lcdc_vga_enabled;
 static int vga_enable_request(int enable)
@@ -2950,6 +2952,12 @@ static int vga_enable_request(int enable)
 
 	return 0;
 }
+#else
+static int vga_enable_request(int enable)
+{
+	return 0;
+}
+#endif
 
 #define GPIO_BACKLIGHT_PWM0 0
 #define GPIO_BACKLIGHT_PWM1 1
@@ -4492,6 +4500,7 @@ cfg_gpio_low_power(struct sx150x_low_power_cfg *cfgs, unsigned nelems)
 	}
 }
 
+#if defined(CONFIG_GPIO_SX150X) || defined(CONFIG_GPIO_SX150X_MODULE)
 static int __init cfg_sx150xs_low_power(void)
 {
 	cfg_gpio_low_power(common_sx150x_lp_cfgs,
@@ -4502,6 +4511,7 @@ static int __init cfg_sx150xs_low_power(void)
 	return 0;
 }
 module_init(cfg_sx150xs_low_power);
+#endif
 
 #ifdef CONFIG_I2C
 static struct i2c_board_info core_expander_i2c_info[] __initdata = {
@@ -6364,6 +6374,7 @@ static struct pmic8058_othc_config_pdata othc_config_pdata_2 = {
 };
 
 
+#if defined(CONFIG_PMIC8058_OTHC) || defined(CONFIG_PMIC8058_OTHC_MODULE)
 static void __init msm8x60_init_pm8058_othc(void)
 {
 	int i;
@@ -6400,7 +6411,7 @@ static void __init msm8x60_init_pm8058_othc(void)
 		}
 	}
 }
-
+#endif
 
 static int pm8058_pwm_config(struct pwm_device *pwm, int ch, int on)
 {
